52b94fede204fa8d62339fa60d3dfd91078ac4f6
aaron-plahn <aplahn85@gmail.com>
2021-02-02 11:13:39 -0800

    feat: Use alphabet lib in coolsite

--- projects/alphabet/src/lib/alphabet.component.ts
+++ projects/alphabet/src/lib/alphabet.component.ts
@@ -1,16 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'lib-alphabet',
-  template: `
-    <p>
-      alphabet works!
-    </p>
-  `,
-  styles: [
-  ]
+  selector: 'alphabet-card',
+  templateUrl: './alphabet.component.html',
+  styleUrls: ['./alphabet.component.css']
 })
 export class AlphabetComponent implements OnInit {
+
+  tile = {
+    'image': 'https://www.tsilhqotin.ca/wp-content/uploads/2020/11/cropped-tng_logo_resized.png',
+    'word': 'tng',
+    'letter': 't'
+  }
 
   constructor() { }
 
